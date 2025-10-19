cecdf3c49f1cb29373711586e48057a3cba512a9
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-10-19 18:02:57 +0000

    Ajout des Sets

--- src/CollectionsManupulations.java
+++ src/CollectionsManupulations.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionsManupulations {
     /**
@@ -17,5 +19,23 @@ public class CollectionsManupulations {
         list.add("B");
         list.add("A"); // doublons permis
         return list;
+    }
+
+
+    /**
+     * L'INTERFACE SET
+     * Pas de doublons
+     * Non indexé.
+     */
+    Set<String> mySets = new HashSet<>();
+
+    public Set<String> getMySets() {
+        Set<String> sets = new HashSet<>();
+        sets.add("A");
+        sets.add("B");
+        sets.add("A");
+        sets.add("A");
+        sets.add("C");
+        return sets;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
         CollectionsManupulations collectionsManupulations = new CollectionsManupulations();
+
+        // List
         List<String> list = collectionsManupulations.getMyList();
         System.out.printf(list.get(1));
+
+        // Set
+        Set<String> sets = collectionsManupulations.getMySets();
+        System.out.println(sets.size());
     }
 }
